56a48c4fcce540e380bb21a50729ee8b82301b4a
NureMiachKateryna <kateryna.miach@nure.ua>
2025-04-09 19:13:25 +0300

    PZ1

---
+++ 2/_2.py
@@ -0,0 +1,8 @@
+﻿# Функція для обертання рядка
+def reverse_string(text):
+    return text[::-1]
+
+# Приклад використання
+original = input("Введи рядок: ")
+reversed_text = reverse_string(original)
+print("Обернений рядок:", reversed_text)
